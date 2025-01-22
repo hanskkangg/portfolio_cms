042a811d72f20bcf91fc4e69fe4761ea7c32107f
hans kang <hans.kkang@gmail.com>
2025-01-22 15:40:15 -0500

    Completed Best Seller section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 {/* Whenever we open home we import the hero component */}
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
+import BestSeller from '../components/BestSeller'
 
 const Home = () => {
   return (
@@ -10,8 +11,8 @@ const Home = () => {
 
       {/* Whenever we open home we import the hero component */}  
       <Hero/>
-
       <LatestCollection/>
+      <BestSeller/>
     </div>
   )
 }
