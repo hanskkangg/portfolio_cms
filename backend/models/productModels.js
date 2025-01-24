df0c9c3f1e0c5baa03d3f5e5201a0d154454b1f8
hans kang <hans.kkang@gmail.com>
2025-01-24 13:16:24 -0500

    productModel created

---
+++ backend/models/productModels.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+
+
+//creating schema
+const productSchema = new mongoose.Schema({
+
+    name: {type:String, required:true }, 
+    description: {type:String, required:true }, 
+    price: {type:Number, required:true }, 
+    image: {type:Array, required:true }, 
+    category: {type:String, required:true }, 
+    subCategory: {type:String, required:true }, 
+    sizes: {type:Array, required:true }, 
+    bestseller :{type: Boolean},
+    date: {type:Number, required:true}
+})
+
+//when product is availbe the model will be used, if its not available it will create one 
+const productModel = mongoose.models.product || mongoose.model("product",productSchema)
+
+export default productModel
