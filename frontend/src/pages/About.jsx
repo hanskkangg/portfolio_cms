19971c668d7e5b298fb7f7185dfca2b59a1625b0
hans kang <hans.kkang@gmail.com>
2025-01-23 22:50:24 -0500

    Started working on about us page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Title from '../components/Title'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className = 'text-2xl text-center pt-8 border-t'>
+      <Title text1={'ABOUT'} text2={'US'}/></div>
   )
 }
 
