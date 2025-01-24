b1d19f8d01cf3c26863a3feb9ae60a92728b20db
hans kang <hans.kkang@gmail.com>
2025-01-24 12:17:43 -0500

    cloudinary storage set up complete

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,18 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
 
 //App config
 const app = express()
 //backend will start on port 4000
 const port = process.env.PORT || 4000
+
+//connecting mongodb.js
 connectDB()
+
+//connecting cloudinary.js
+connectCloudinary()
 
 //middlewares
 app.use(express.json())
