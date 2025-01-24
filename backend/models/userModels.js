a0c664175321beddc3c29f2916b5796b51bf323e
hans kang <hans.kkang@gmail.com>
2025-01-24 13:21:32 -0500

    userModels created

---
+++ backend/models/userModels.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import { isEmpty } from "validator";
+
+//creating user schema
+
+const userSchema = new mongoose.Schema({
+
+    name: {type:String, required:true},
+    //email is set to unique meaning one email can only be assinged to one account
+    email: {type:String, required:true, unique:true},
+    password: {type:String, required:true},
+    //default value is empty for empty cart
+    cartData: {type:Object, default:{}}
+
+
+
+
+}, {minimize:false})
+
+
+const userModel = mongoose.models.user || mongoose.model('user', userSchema);
+
+export default userModels
