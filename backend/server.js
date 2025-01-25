7126e75e5eda51c8f0fc2e66c64956023b65c946
hans kang <hans.kkang@gmail.com>
2025-01-25 10:38:40 -0500

    multer for storage complete

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoute.js'
+import productRouter from './routes/productRoute.js'
 
 //App config
 const app = express()
@@ -20,12 +21,15 @@ connectCloudinary()
 app.use(express.json())
 app.use(cors())
 
-app.use('/api/user',userRouter)
+app.use('/api/user', userRouter)
+app.use('/api/product', productRouter)
 
 // API endpoints
 app.get('/',(req,res)=>{
     res.send("my API is working!")
 })
+
+
 
 app.listen(port, ()=> console.log('Server has started on PORT: ' + port)
 )
