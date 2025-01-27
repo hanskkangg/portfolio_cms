0f2f41c61524043d6a1da1c24971ffe1fb014703
hans kang <hans.kkang@gmail.com>
2025-01-27 13:26:02 -0500

    Sidebar complet and font has been added

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 import React from 'react'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
 
 function App() {
 
   return (
     <>
+    <Navbar/>
+    <Sidebar/>
     
     </>
   )
--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import React from 'react'
-import ReactDOM from 'react-router-dom'
 import {BrowserRouter} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
