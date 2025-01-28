adbbb6e5baa30e2add2331ae534f4a755a78afe9
hans kang <hans.kkang@gmail.com>
2025-01-28 11:12:40 -0500

    Started working on list page

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { backendUrl, currency } from '../App'
+import { toast } from 'react-toastify'
 
 const List = () => {
   return (
