113b5e2999cbb3b6c9f59208a937160a119f196a
hans kang <hans.kkang@gmail.com>
2025-03-20 15:00:15 -0400

    Footer updated for xl and lg

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -9,10 +9,10 @@ const Footer = ({ isDarkMode }) => {
       <div className="flex flex-col sm:grid sm:grid-cols-3 gap-8 text-sm text-center sm:text-left">
         
         {/* Logo & Description */}
-        <div className="flex flex-col-2 items-center sm:items-start space-y-4 xl:mt-10">
+        <div className="flex flex-col-2 items-center sm:items-start space-y-4 xl:mt-10 lg:mt-10">
           <img  
             src={isDarkMode ? assets.baeyondnail : assets.baeyondnail} 
-            className="w-20 transition-all duration-300 mt-4" 
+            className="w-20 transition-all duration-300 mt-4 lg:mt-7 xl:mt-4" 
             alt="Baeyond Nails Logo" 
           />
           <p className="w-full max-w-md text-gray-600 dark:text-gray-300">
@@ -45,7 +45,8 @@ const Footer = ({ isDarkMode }) => {
       {/* Footer Bottom Section */}
       <div className="mt-10">
         <hr className="border-gray-300 dark:border-gray-700" />
-        <p className="py-5 text-sm text-center text-gray-600 dark:text-gray-100 xl:mb-[-3%]">
+        <p className="py-5 text-sm text-center text-gray-600 dark:text-gray-100 xl:mb-[-3%]
+        lg:mb-[-5%]">
           Copyright 2024 © Baeyond Nails - All Rights Reserved.
         </p>
       </div>
