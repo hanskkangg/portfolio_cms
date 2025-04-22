19b66a81aeb56115429f21108caace191a06245c
hans kang <hans.kkang@gmail.com>
2025-04-21 23:37:16 -0400

    chore: remove unused pages for CMS personal portfolio

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,16 +5,18 @@ import Collection from './pages/Collection';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Product from './pages/Product';
-import Cart from './pages/Cart';
-import Login from './pages/Login';
-import PlaceOrders from './pages/PlaceOrders';
-import Orders from './pages/Orders';
+
+// import Cart from './pages/Cart';
+// import Login from './pages/Login';
+// import PlaceOrders from './pages/PlaceOrders';
+// import Orders from './pages/Orders';
+// import Verify from './pages/Verify';
+
 import Navbar from './components/NavBar';
 import Footer from './components/Footer';
 import SearchBar from './components/SearchBar';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Verify from './pages/Verify';
 import './sparkles.css';
 
 import TurnstileVerify from "./components/TurnstileVerify"; 
@@ -104,11 +106,14 @@ const App = () => {
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/product/:productId' element={<Product />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/place-order' element={<PlaceOrders />} />
-        <Route path='/orders' element={<Orders />} />
-        <Route path='/verify' element={<Verify />} />
+
+
+        {/* Remove unused pages */}
+        {/* <Route path='/cart' element={<Cart />} /> */}
+        {/* <Route path='/login' element={<Login />} /> */}
+        {/* <Route path='/place-order' element={<PlaceOrders />} /> */}
+        {/* <Route path='/orders' element={<Orders />} /> */}
+        {/* <Route path='/verify' element={<Verify />} /> */}
       </Routes>
       <Footer isDarkMode={isDarkMode} />
         </>
